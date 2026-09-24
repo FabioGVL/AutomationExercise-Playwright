11513e2dabbc07fd1f6c8c6e41e9679a8e6a8a19
FabioGVL <135769523+FabioGVL@users.noreply.github.com>
2026-09-24 12:13:25 -0300

    AutomationExercise - Playwright

--- suporte/elementos/pageElements.js
+++ suporte/elementos/pageElements.js
@@ -157,7 +157,7 @@ class PageElements {
 
     // --- OUTRAS FUNCIONALIDADES ---
     this.btnScrollUp = page.locator('#scrollUp');
-    this.tituloHeader = page.getByRole('heading', { name: 'Full-Fledged practice website for Automation Engineers' });
+    this.tituloHeader = page.locator('#slider-carousel .item.active h2');
     this.sliderCarousel = page.locator('#slider');
   }
 }
